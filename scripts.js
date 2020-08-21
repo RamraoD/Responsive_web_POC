fce4322160522a0702cd2eaa967c1be6edb0b84b
chaitanya.nadiger <chaitanya.nadiger@innso.com>
2020-08-21 10:59:57 +0530

    Fixing slider

--- scripts.js
+++ scripts.js
@@ -1,44 +1,50 @@
-$(document).ready(function(){
-  $("#navbarCollapse a").on('click', function(event) {
+$(document).ready(function () {
+  initGallery();
+
+  $("#navbarCollapse a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-        window.location.hash = hash;
-      });
-    }  
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        900,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
   });
 });
 
-var slideIndex,slides,dots,captionText;
+var slideIndex, slides, dots, captionText;
 function initGallery() {
-  slideIndex=0;
-  slides=document.getElementsByClassName("imageholder");
-  console.log(slides)
-  slides[slideIndex].style.opacity=1;
-  console.log(slideIndex)
+  slideIndex = 0;
+  slides = document.getElementsByClassName("imageholder");
+  console.log(slides);
+  slides[slideIndex].style.opacity = 1;
+  console.log(slideIndex);
 
-  captionText=document.querySelector(".captiontHolder.captionText");
-  captionText.innerHTML=slides(slideIndex).querySelector(".captionText").innerHTML;
+  captionText = document.querySelector(".captiontHolder.captionText");
+  captionText.innerHTML = slides(slideIndex).querySelector(
+    ".captionText"
+  ).innerHTML;
 
-  dots=[];
-  var dotsContainer=document.getElementById("dotsContainer");
+  dots = [];
+  var dotsContainer = document.getElementById("dotsContainer");
 
-  for(var i=0;i<slides.length;i++)
-  var dot=document.createElement("span");
+  for (var i = 0; i < slides.length; i++)
+    var dot = document.createElement("span");
   dot.classList.add("dots");
   ditsContainer.append(dot);
   dots.push(dot);
 }
-initGallery();
 
-function  moveSlide(n) {
-  var i,current,next;
-  var moveSlideAnimClass= {
-    forCurrent:"",
-    forNext:""
-  }
+function moveSlide(n) {
+  var i, current, next;
+  var moveSlideAnimClass = {
+    forCurrent: "",
+    forNext: "",
+  };
 }
-
